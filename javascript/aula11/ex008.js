5283804226c08370fd33b46ffde5218dff92d0c6
GuilhermeCampo <guilherme.hcampo@gmail.com>
2024-07-08 19:39:14 -0300

    exercicios javascript

---
+++ javascript/aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 70
+console.log(`A valocidade do carro é ${vel}km/h`)
+if (vel >60){   //Condição simples, não tem o 'else'
+  console.log('Você ultrapassou a velocidade máxima permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando sinto de segurança')
