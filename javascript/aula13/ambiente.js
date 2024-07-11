0d1a83ea661e334fa96e603808ed3c03240af255
GuilhermeCampo <guilherme.hcampo@gmail.com>
2024-07-11 11:45:00 -0300

    Create ambiente.js

---
+++ javascript/aula13/ambiente.js
@@ -0,0 +1,32 @@
+
+var c = 1
+
+do {
+    console.log(`Contador ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+
+
+
+
+
+
+// var c = 1
+
+// while (c <= 500){
+//     console.log(`Passo ${c}`)
+//     c++
+// }
+
+
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
