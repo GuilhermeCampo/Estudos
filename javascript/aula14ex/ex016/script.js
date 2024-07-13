8a83f28f329ac20f78bec14c4b0b181eb0268136
GuilhermeCampo <guilherme.hcampo@gmail.com>
2024-07-13 15:18:27 -0300

    js ex 16

--- javascript/aula14ex/ex016/script.js
+++ javascript/aula14ex/ex016/script.js
@@ -1,11 +1,45 @@
 function calcular(){
-    var inicio = document.querySelector('input#inicio')
-    var fim = document.querySelector('input#fim')
-    var passo= document.querySelector('input#passo')
+
+    let ini = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let step = document.getElementById('passo')
+    let res = document.querySelector('div#res')
 
 
-    var res = document.getElementById('res')
-    for (var i=inicio; i<fim ;i += passo){
-        res.innerHTML=i
-    }
-}
+
+        if (ini.value.length == 0 || fim.value.length == 0 || step.value.length == 0){
+            window.alert('[ERRO] Há dados faltando!')
+        } else {
+
+                    
+                    res.innerHTML=`Calculando...`
+                    let i = Number(ini.value)
+                    let f = Number(fim.value)
+                    let p = Number(step.value)
+
+                    if (p == 0){
+                        window.alert('Passo inválido. Considerando passo =1')
+                        p = 1
+                        
+                    }
+
+                if (i < f){
+                    for (let c = i; c <= f ; c += p){
+                        res.innerHTML += ` \u{1F449} ${c}  `
+                    }
+                    res.innerHTML += `\u{1F3C1}`
+                } else {
+                    for ( let c = i; c>=f; c -= p){
+                        res.innerHTML += `\u{1f449} ${c}`
+                    }
+                     res.innerHTML += `\u{1F3C1}`
+                }
+            
+                } 
+            
+            
+
+
+
+
+        }
--- javascript/aula14ex/ex017/script.js
+++ javascript/aula14ex/ex017/script.js
@@ -1,1 +1,14 @@
-alert('Olá')
+function calc(){
+
+    let num = document.getElementById('num')
+    let tab =  document.getElementById('selTab')
+
+    if (num.value.length == 0){
+        window.alert('Digite um número')
+    }else{
+
+
+    let n = Number(num.value)
+
+    }
+}
