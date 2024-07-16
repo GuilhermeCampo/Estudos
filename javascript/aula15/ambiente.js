b11a7aa2bf6aa7661165d26640668c6ebc2cdeaf
GuilhermeCampo <guilherme.hcampo@gmail.com>
2024-07-16 11:47:24 -0300

    Create ambiente.js

---
+++ javascript/aula15/ambiente.js
@@ -0,0 +1,15 @@
+let num = [8, 2, 7, 1, 9]
+num.sort()
+
+
+// for (let pos = 0; pos <= num.length; pos++){
+//     console.log(num[pos])
+// }
+
+for (let pos in num){
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
+
+
+// num.length   -> tamanho da array, quantidade de elementos
+//num.sort() -> ordenação em ordem crescente
