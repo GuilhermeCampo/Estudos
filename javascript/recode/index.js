3054cbecb02b5d04f7a2121f06f3d6909e0b1245
GuilhermeCampo <guilherme.hcampo@gmail.com>
2024-07-25 18:41:43 -0300

    Update index.js

--- javascript/recode/index.js
+++ javascript/recode/index.js
@@ -1,6 +1,15 @@
-let frutas = []
+function logComHora(textoEntrada){
 
- 
- frutas.push('banana', 'morango', 'maçã', 'limão', 'carambola', 'abacate')
-frutas.shift()
-console.log(frutas)
+    let data = new Date()
+    let hora = data.getHours()
+    let min = data.getMinutes()
+    let sec = data.getSeconds()
+
+    let hora_formatada = `${hora}:${min}:${sec}`
+    console.log(`${hora_formatada} : ${textoEntrada}`)
+
+
+}
+
+logComHora('Um texto qualquer')
+
